68a1aa1d07e661a9edab54543d01053ba0417fd1
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-10-23 17:17:10 +0300

    Dictionary

--- 16_Dictionary/Program.cs
+++ 16_Dictionary/Program.cs
@@ -1,129 +1,76 @@
 ﻿using System;
 
 namespace _16_Dictionary
-{
-    public delegate int ArrayDelegate();
-    public delegate void VoidDelegate();
-
-    class Array
-    {
-        int[] array = new int[] { 1, 12, 3, 13 ,-17, -10, -24, -35 };
-        public int NegativeNumbers()
-        {
-            int a = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] < 0)
-                {
-                    ++a;
-                   // Console.WriteLine(a + " " + array[i]);
-                }
-
-            }
-            return a;
-        }
-        public void Print()
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i] + " ");
-            }
-        }
-        public int SumNumber()
-        {
-            return array.Sum();
-        }
-        public int EasyNumbers()
-        {
-            int a = 0;
-            for (int i = 2; i <= array.Length / 2; i++)
-            {
-                if (array[i] % i == 0)
-                {
-                    a++;
-                }
-            }
-            return a;
-        }
-        public void ChangingNegativeNumbers()
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] < 0)
-                {
-                    array[i] = 0;
-                }
-            }
-        }
-        public void SortArray()
-        {
-            System.Array.Sort(array);
-        }
-        public void MovingElementsArray()//переміщення елементів у масиві
-        {
-            int left = 0;
-            int right = array.Length - 1;
-            while (left < right)
-            {
-                while (left < right && array[left] % 2 == 0)
-                {
-                    left++;
-                }
-
-                while (left < right && array[right] % 2 != 0)
-                {
-                    right--;
-                }
-
-                if (left < right)
-                {
-                    int temp = array[left];
-                    array[left] = array[right];
-                    array[right] = temp;
-                    left++;
-                    right--;
-                }
-            }
-        }
-    }
-    static class MyClass
-    {
-        //public static string GoToPassword(this string s, int key)
-        //{
-        //    char[] a = s.ToCharArray();
-        //    for (int i = 0; i < a.Length; i++)
-        //    {
-        //        a[i] = a[i] + char.Parse(key);
-        //    }
-        //    string res = a.ToString();
-        //    return res;
-        //}
-    }
+{  
     internal class Program
-    {
-        
+    {        
         static void Main(string[] args)
         {
-            Array array = new Array();
-            array.Print();
-            array.MovingElementsArray();
-            array.Print();
+            Dictionary<string,string> countries = new Dictionary<string,String>();
 
-            ArrayDelegate delegate1 = new ArrayDelegate(array.NegativeNumbers);
-            delegate1 += array.SumNumber;
-            delegate1 += array.EasyNumbers;
+            countries.Add("UA", "Ukraine");
+            countries.Add("PL", "Poland");
+            countries.Add("USA", "United States");
+            countries.Add("FR", "France");
+            countries.Add("GB", "Great Britain");
 
-            Console.WriteLine("1- NegativeNumbers;2 - SumNumber ; 3 - EasyNumbers;");
-            int key = int.Parse(Console.ReadLine());
+            foreach (KeyValuePair<string,string> item in countries)
+            {
+                Console.WriteLine($"Key : {item.Key, 7} . Value : {item.Value,15}");
 
-            var  collection = delegate1.GetInvocationList();
-            Console.WriteLine((collection[key-1] as ArrayDelegate).Invoke()); ;
-            //foreach (var item in delegate1.GetInvocationList())
-            //{
-            //    Console.WriteLine((item as ArrayDelegate).Invoke());
-            //}
+            }
+
+            var country = countries["USA"];//get
+            Console.WriteLine(country);
+            countries["USA"] = "America";//set
+            countries["IN"] = "India";//set
+            countries["RUN"] = "RUNNING";//set
+            countries.Remove("FR");
+            Console.WriteLine();
+            foreach (KeyValuePair<string, string> item in countries)
+            {
+                Console.WriteLine($"Key : {item.Key,7} . Value : {item.Value,15}");
+            }
+            Console.WriteLine();
+            Dictionary<char,Person> people = new Dictionary<char,Person>();
+            people.Add('A', new Person() { Name = "Andriy" });
+            people.Add('P', new Person() { Name = "Petro" });
+            people.Add('M', new Person() { Name = "Mukola" });
+            people.Add('O', new Person() { Name = "Olga" });
+            // people.Add('A', new Person() { Name = "Anna" }); // exception
+           
+
+            foreach (var item in people)
+            {
+                Console.WriteLine($"Key : {item.Key,7} . Value : {item.Value.Name,15}");
+            }
+
+            if(people.ContainsKey('A'))
+            {
+                people['A'] = new Person() { Name = "Anna" };
+                Console.WriteLine("Info was changed!!!!");
+            }
+            foreach (var item in people)
+            {
+                Console.WriteLine($"Key : {item.Key,7} . Value : {item.Value.Name,15}");
+            }
+
+            foreach (var item in people.Keys)
+            {
+                Console.WriteLine("Key : " + item);
+            }
+
+            foreach (var item in people.Values)
+            {
+                Console.WriteLine("Value : " + item.Name);
+            }
 
 
+        }
+
+        class Person
+        {
+            public string Name { get; set; }
         }
     }
 }
